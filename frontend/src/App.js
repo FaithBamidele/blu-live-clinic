5bd42a4fad4e17f6e39b299982311e31d0c2a98e
Faith Bamidele <168541654+FaithBamidele@users.noreply.github.com>
2026-04-26 17:40:19 +0100

    Update API URL to backend endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API = process.env.REACT_APP_API_URL;
+const API = "https://blc-backend-app.azurewebsites.net/api";
 
 const doctorPhotos = {
   "Faith Bitrus": "https://lh3.googleusercontent.com/d/140WAJOrcnvvZlj9Wb5WYXz1tj7vwWj8E", 
